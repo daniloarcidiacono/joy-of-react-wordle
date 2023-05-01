8c6deb694fe79364516022f5a392f68984c2be28
Danilo Arcidiacono <danilo.arcidiacono@gmail.com>
2023-05-01 11:19:29 +0200

    fix(exercise 1): fixes after watching solution.

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -13,10 +13,14 @@ function GuessInput({ onSubmit }) {
     <form className="guess-input-wrapper" onSubmit={submitGuess}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input id="guess-input"
-             type="text" 
-             value={guess} 
-             onChange={event => setGuess(sanitizeGuess(event.target.value))} 
-             pattern="^[A-Z]{5}$" 
+        type="text" 
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="^[A-Z]{5}$" 
+        title="5 letter word"
+        value={guess} 
+        onChange={event => setGuess(sanitizeGuess(event.target.value))}              
       />
     </form>
   );
