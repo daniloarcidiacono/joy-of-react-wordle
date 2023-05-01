6ebc812faf4263b1e94d7952f8e4f479069a84ab
Danilo Arcidiacono <danilo.arcidiacono@gmail.com>
2023-05-01 11:26:36 +0200

    feat(exercise 2): add GuessResults component.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,19 +3,25 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
+import GuessResults from '../GuessResults/GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
+
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
   const logGuess = guess => {
-    console.log(guess);
-  };
+    const nextGuesses = [...guesses];
+    nextGuesses.push(guess);
+    setGuesses(nextGuesses);
+  }
   
   return (
     <>
+      <GuessResults guesses={guesses} />
       <GuessInput onSubmit={logGuess} />
     </>
   );
